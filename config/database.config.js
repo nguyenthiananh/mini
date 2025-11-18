c48d259e27677d4bdb03f8f0be61919d969fcc4a
nguyenthilananh <ntlanha24069@cusc.ctu.edu.vn>
2025-11-18 15:08:37 +0700

    second commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://:ttthinha24062:ttthinha24062@cluster0.fgvh56z.mongodb.net/?appName=Cluster0');
+        await mongoose.connect('mongodb+srv://ntlanha24069:ntlanha24069@cluster0.i9o1ibk.mongodb.net/?appName=Cluster0');
         console.log('Kết nối MongoDB thành công.');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB: ', err);
